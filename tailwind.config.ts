a78fdfa7a392c43cb498ae816ab1251c43b17a9e
arLong-98 <dhairya999pateriya@gmail.com>
2023-10-02 19:55:32 +0530

    fix: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      "tinted-black": "#14151D",
+      purple: "#7940FF",
+      gray: "#C4C4C4",
+      "pill-gray": "#8A898D",
+      "pill-bg-gray": "#F4F4F8",
+      "light-gray": "#51525C",
+      "dark-gray": "#212229",
+      "footer-gray": "#929292",
+      blue: "#4075FF",
+      pink: "#FF4085",
+      "break-grey": "#FAFAFC",
+      offWhite: "#F5F5F5",
+      transparent: "transparent",
+      errorRed: "#E97061",
+      veryLightGrey: "#eeeeee",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        blurGradient:
+          "linear-gradient(270deg, rgba(22,23,25,0.7497592787114846) 100%, rgba(255,255,255,0) 100%)",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
 }
-export default config
+
+export default config;

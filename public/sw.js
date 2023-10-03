d4a94c33300abecf1f03648635e3b241cc555038
arLong-98 <dhairya999pateriya@gmail.com>
2023-10-03 23:34:24 +0530

    fix: update service worker file to cache index page

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gamezop-app";
+const CACHE_NAME = "gamezop-app-v1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -9,22 +9,22 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     (async () => {
-//       const cache = await caches.open(CACHE_NAME);
-//       const cachedResponse = await cache.match(event.request);
-//       if (cachedResponse) {
-//         return cachedResponse;
-//       } else {
-//         try {
-//           const fetchResponse = await fetch(event.request);
-//           cache.put(event.request, fetchResponse.clone());
-//           return fetchResponse;
-//         } catch (e) {
-//           // The network failed.
-//         }
-//       }
-//     })()
-//   );
-// });
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      } else {
+        try {
+          const fetchResponse = await fetch(event.request);
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+      }
+    })()
+  );
+});

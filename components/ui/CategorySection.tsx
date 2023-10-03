3fbad2daf0909bcb7105d1001a2c4d0173b2b870
arLong-98 <dhairya999pateriya@gmail.com>
2023-10-03 23:30:43 +0530

    fix: update scrollbars to be hidden, add a utility to tailwind

--- components/ui/CategorySection.tsx
+++ components/ui/CategorySection.tsx
@@ -2,7 +2,7 @@ function CategorySection({ sectionTitle, children }: CategorySectionProps) {
   return (
     <section className="flex flex-col py-10 gap-5">
       <h2 className="text-4xl md:text-5xl">{sectionTitle}</h2>
-      <div className="flex flex-col tablet:flex-row tablet:max-laptop:overflow-scroll laptop:flex-wrap tablet:gap-x-[20px] laptop:gap-x-[24px] gap-y-[30px]">
+      <div className="no-scrollbar flex flex-col tablet:flex-row tablet:max-laptop:overflow-scroll laptop:flex-wrap tablet:gap-x-[20px] laptop:gap-x-[24px] gap-y-[30px]">
         {children}
       </div>
     </section>
